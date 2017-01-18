90efbe7619a0812d21129679e67209d639f330de
Ryan Mulcahy <ryan@primeacademy.io>
2017-01-18 15:24:11 -0600

    Finish lecture

--- public/client.js
+++ public/client.js
@@ -1,14 +1,43 @@
 $(function(){
   console.log('document loaded');
 
+  getSongs();
+
+  $('#addSong').on('submit', addSong);
+});
+
+function getSongs() {
   $.ajax({
-    url: '/data',
+    url: '/songs',
     type: 'GET',
     success: displaySongs
   });
+}
 
-});
+function addSong(event) {
+  // stop the browser from trying to navigate away from our page
+  event.preventDefault();
+
+  // get the information out of the form
+  var songData = $(this).serialize();
+
+  console.log(songData);
+
+  $.ajax({
+    url: '/songs',
+    type: 'POST',
+    data: songData,
+    success: getSongs
+  })
+}
 
 function displaySongs(songs) {
   console.log(songs);
+
+  $('#songs').empty();
+
+  songs.forEach(function(song) {
+    $('#songs').append('<li>' + song.title + ' by '
+      + song.artist + ' from album ' + song.album + '</li>');
+  });
 }
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+
 var songs = require('./data.json');
 
 var app = express();
 
 app.use(express.static('public'));
+// convert any url encoded body into a JS object
+// added to req.body
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/data', function(req, res) {
+app.get('/songs', function(req, res) {
   res.send(songs);
 });
+
+app.post('/songs', function(req, res) {
+  console.log('req.body', req.body);
+  songs.push(req.body);
+  res.sendStatus(200);
+})
 
 app.listen(3000);
